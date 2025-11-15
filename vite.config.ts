489b551c05469fa0760d7fca9fe3fcd323d09e31
heqian <182359041@qq.com>
2025-11-15 20:18:11 +0800

    refactor: 更新脚本路径和配置 chore: 添加.gitignore文件 perf: 优化构建脚本和tsconfig配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    root: '.', // 项目根
+    server: {
+        port: 5173
+    },
+    build: {
+        outDir: 'dist',
+        lib: {
+            entry: resolve(__dirname, 'src/image-cropper.ts'),
+            name: 'ImageCropperLib',
+            fileName: (format) => `image-cropper.${format}.js`,
+            formats: ['es', 'umd'] // es 给模块化环境；umd 给浏览器 script 直接引用
+        },
+    },
+
+});
